6f4382ad4004e083159e9672e07b2980300f8b4d
Kadir Zeren <k4dzeren@gmail.com>
2025-12-04 23:49:54 +0300

    comparison logical operators

--- js/operators.js
+++ js/operators.js
@@ -75,4 +75,58 @@ let sayi=45.986
 console.log(Math.trunc(sayi))
 console.log(sayi)
 
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                            COMPARISON OPERATORS                            */
+/* -------------------------------------------------------------------------- */
+
+// Karşılaştırma operatörleri bize Boolean sonuç döndürür.
+
+let c=2
+let d="2"
+
+console.log(typeof c)
+console.log(typeof d)
+console.log(c==d)
+console.log(c===d)
+
+console.log(c!=d) // eşit değil mi - false 2  2 ye eşit
+console.log(c!==d) // eşit değil mi - true biri string biri number olduğu için eşit değil
+console.log(3>=2)
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                              LOGICAL OPERATORS                             */
+/* -------------------------------------------------------------------------- */
+
+//! AND  - && ( ve anlamındadır)
+// ANd mükemmeliyetçidir
+// Herşey true olmalı  yoksa true değer dönmez
+
+console.log(Boolean("Merhaba") )
+console.log(Boolean(0))
+console.log(Boolean(2))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean([]))
+
+console.log([] && 0 && 2)
+console.log([] && "M" && 5)
+
+console.clear()
+//! OR - ||
+// Herşey false olsa bile bir tane true, true döndermeye yeter
+// Hepsi false ise en son false değerini verir
+
+console.log(0 || null || 2 ||  undefined )
+
+
+console.log(!null)
+
+
+
+
+
+
+
 
